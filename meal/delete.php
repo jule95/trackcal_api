dde992bcd1b7f48c1cf3b2005e90ff5b89297999
Julius Plass <julius.plass@hotmail.com>
2020-07-17 15:13:51 +0200

    implemented setter

--- meal/delete.php
+++ meal/delete.php
@@ -11,27 +11,35 @@ header("Access-Control-Allow-Origin: *");
 //set format of retrieved data to json and characters set to utf8
 header("Content-Type: application/json; charset=UTF-8");
 //specify method which can be used to access this resource
-header("Access-Control-Allow-Methods: DELETE"); //POST instead of DELETE???
+header("Access-Control-Allow-Methods: DELETE");
 //???
 header("Access-Control-Max-Age: 3600");
 //indicate which headers can actually be used to send this post request
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
+//include database configuration to get connection
 include_once "../config/Database.php";
+//include product meal project to perform a create
 include_once "../objects/Meal.php";
 
+//instantiate database
 $database = new Database();
+//create new connection to database
 $conn = $database->getConnection();
 
-$meal = new Meal($conn);
-
+//get post data
 $data = json_decode(file_get_contents("php://input"));
 
+//check if data is available
 $dataIsOk = !empty($data->id);
 
 if ($dataIsOk)
 {
-    $meal->id = $data->id;
+    //create a new meal
+    $meal = new Meal($conn);
+
+    //set properties from received data
+    $meal->setId($data->id);
 
     $mealWasDeleted = $meal->delete();
 
