38d415311d8781d5967d3658943a714ce51d14cc
Julius Plass <julius.plass@hotmail.com>
2020-07-17 15:46:21 +0200

    small changes

--- meal/read.php
+++ meal/read.php
@@ -49,16 +49,16 @@ if ($num > 0)
     // set response code - 200 OK
     http_response_code(200);
 
-    // show products data in json format
+    // show meals data in json format
     echo json_encode($mealsArr);
 } else
 {
     // set response code - 404 Not found
     http_response_code(404);
 
-    // tell the user no products found
+    // tell the user no meals found
     echo json_encode([
-        "message" => "no products found",
+        "message" => "no meals found",
         "status" => 404
     ]);
 }
