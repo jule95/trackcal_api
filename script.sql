5c8830317652ac294daea2516324ea2b69f3c3b2
Julius Plass <julius.plass@hotmail.com>
2020-07-17 15:06:28 +0200

    updated column name for description

--- script.sql
+++ script.sql
@@ -4,7 +4,8 @@ SET sql_safe_updates = 0;
 
 -- table config
 
-ALTER TABLE meal AUTO_INCREMENT = 1;
+ALTER TABLE meal
+    AUTO_INCREMENT = 1;
 
 -- creating database
 DROP DATABASE IF EXISTS trackcal;
@@ -19,28 +20,30 @@ USE trackcal;
 -- create tables
 DROP TABLE IF EXISTS meal;
 
-CREATE TABLE meal (
-                      id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                      name VARCHAR(32) NOT NULL,
-                      calories INT UNSIGNED NOT NULL
+CREATE TABLE meal
+(
+    id       INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    description     VARCHAR(32)  NOT NULL,
+    calories INT UNSIGNED NOT NULL
 );
 
 -- test data
-INSERT INTO meal (name, calories)
-VALUES("Burger", 800);
+INSERT INTO meal (description, calories)
+VALUES ("Burger", 800);
 
-INSERT INTO meal (name, calories)
-VALUES("Pizza", 700);
+INSERT INTO meal (description, calories)
+VALUES ("Pizza", 700);
 
-INSERT INTO meal (name, calories)
-VALUES("Steak", 1000);
+INSERT INTO meal (description, calories)
+VALUES ("Steak", 1000);
 
-INSERT INTO meal (name, calories)
-VALUES("Ham & Eggs", 400);
+INSERT INTO meal (description, calories)
+VALUES ("Ham & Eggs", 400);
 
-INSERT INTO meal (name, calories)
-VALUES("Chicken", 900);
+INSERT INTO meal (description, calories)
+VALUES ("Chicken", 900);
 
 -- validate
 
-SELECT * FROM meal;
+SELECT *
+FROM meal;
