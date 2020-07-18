a9dd00f6144490bcedd48f8aadb54a3b8dad0c23
Julius Plass <julius.plass@hotmail.com>
2020-07-18 12:16:37 +0200

    code cleanup

--- meal/delete.php
+++ meal/delete.php
@@ -6,69 +6,64 @@
  * Time: 13:33
  */
 
-//allow all origins to access this resource
+//CORS headers
 header("Access-Control-Allow-Origin: *");
-//set format of retrieved data to json and characters set to utf8
-header("Content-Type: application/json; charset=UTF-8");
-//specify method which can be used to access this resource
 header("Access-Control-Allow-Methods: DELETE");
-//???
-header("Access-Control-Max-Age: 3600");
-//indicate which headers can actually be used to send this post request
+header("Access-Control-Max-Age: 3600");//?
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+//Other
+header("Content-Type: application/json; charset=UTF-8");
 
-//include required resources
+//Include all required resources.
 include_once "../config/Database.php";
 include_once "../objects/Meal.php";
 include_once "../helper/Response.php";
 
-//instantiate database
-$database = new Database();
-//create new connection to database
-$conn = $database->getConnection();
-
-//get post data
-$data = json_decode(file_get_contents("php://input"));
-
-//check if data is available
-$dataIsOk = !empty($data->id);
-
 if ($_SERVER["REQUEST_METHOD"] === "DELETE")
 {
+    //Create new database object and establish a connection.
+    $database = new Database();
+    $conn = $database->getConnection();
+
+    //Store received request data in JSON and check if data is valid.
+    $data = json_decode(file_get_contents("php://input"));
+    $dataIsOk = !empty($data->id);
+
     if ($dataIsOk)
     {
-        //create a new meal
+        //Create new meal object and pass $conn object used to execute queries.
         $meal = new Meal($conn);
 
-        //set properties from received data
+        //Set relevant properties and delete meal and store result of query.
         $meal->setId($data->id);
-
         $mealWasDeleted = $meal->delete();
 
+        //Check if query executed successfully.
         if ($mealWasDeleted["bool"])
         {
+            //Check if any rows were deleted.
             if ($mealWasDeleted["rowCount"] > 0)
             {
-                //send success response
+                //Send success response.
                 Response::sendResponse(true, null, 200, null);
             } else
             {
-                //send failure response because no rows returned and deleted
+                //Send failure response because no rows returned and deleted.
                 Response::sendResponse(false, "0 rows returned", 404, null);
             }
 
         } else
         {
-            //send failure response because service is unavailable
+            //Send failure response because service is unavailable.
             Response::sendResponse(false, "service unavailable", 503, null);
         }
     } else
     {
-        //send failure response because of incomplete data
+        //Send failure response because of incomplete data.
         Response::sendResponse(false, "incomplete data", 400, null);
     }
 } else
 {
-    //send failure response because of unallowed method
+    //Send failure response because of method is not allowed.
     Response::sendResponse(false, "method not allowed", 405, null);
 }
